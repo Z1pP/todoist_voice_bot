d51811e06c9f41c023162209aabadd5888162b75
Z1pP <maxm_99@mail.ru>
2025-04-19 18:36:32 +0300

    added repository to start handler

--- bot/handlers/command_handler.py
+++ bot/handlers/command_handler.py
@@ -1,10 +1,11 @@
-from aiogram import Router
+from aiogram import F, Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
 from bot.constants import MESSAGES
 from bot.keyboards.reply import reply_kb
 from bot.utils import send_message_with_keyboard
+from database.repositories import TgProfileRepository
 
 router = Router(name=__name__)
 
@@ -14,7 +15,17 @@ async def start_command(message: Message):
     """
     Обработчик команды /start
     """
-    await send_message_with_keyboard(message, MESSAGES["start"], reply_kb.menu)
+    tg_id = message.from_user.id
+
+    try:
+        # TODO: Вынести в отдельный сервис (UseCase)
+        repository = TgProfileRepository()
+        user = repository.get_by_tgid(tg_id)
+        if not user:
+            user = repository.add_new_profile(tg_id)
+        await send_message_with_keyboard(message, MESSAGES["start"], reply_kb.menu_btn)
+    except Exception as e:
+        await send_message_with_keyboard(message, str(e))
 
 
 @router.message(Command("help"))
@@ -23,3 +34,11 @@ async def help_command(message: Message):
     Обработчик команды /help
     """
     await send_message_with_keyboard(message, MESSAGES["help"])
+
+
+@router.message(F.text)
+async def echo(message: Message):
+    """
+    Обработчик текстовых сообщений
+    """
+    await send_message_with_keyboard(message, MESSAGES["echo"])
