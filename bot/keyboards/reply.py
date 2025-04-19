baaecfff34f97b58caac1119e163b8459904fcfb
Z1pP <maxm_99@mail.ru>
2025-04-19 18:37:23 +0300

    refactoring reply kbs

--- bot/keyboards/reply.py
+++ bot/keyboards/reply.py
@@ -2,7 +2,13 @@ from dataclasses import dataclass
 
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
-from bot.constants import ANSWERS, INPUT_METHODS, MENU
+from bot.constants import (
+    Answers,
+    CommonButtons,
+    InputMethod,
+    MenuButtons,
+    SettingsButtons,
+)
 
 
 @dataclass
@@ -12,8 +18,12 @@ class ReplyKeyboards:
     """
 
     @property
-    def menu(self):
-        buttons = [KeyboardButton(text=value) for key, value in MENU.items()]
+    def menu_btn(self):
+        buttons = [
+            KeyboardButton(text=button.value)
+            for button in MenuButtons
+            if button not in (MenuButtons.BACK, MenuButtons.CANCEL)
+        ]
         return ReplyKeyboardMarkup(
             keyboard=[
                 buttons[:3],
@@ -24,27 +34,37 @@ class ReplyKeyboards:
         )
 
     @property
-    def type_input(self):
+    def type_input_btn(self):
+        return ReplyKeyboardMarkup(
+            keyboard=[[KeyboardButton(text=button.value) for button in InputMethod]],
+            resize_keyboard=True,
+            one_time_keyboard=True,
+        )
+
+    @property
+    def answer_btn(self):
+        return ReplyKeyboardMarkup(
+            keyboard=[KeyboardButton(text=button.value) for button in Answers],
+            resize_keyboard=True,
+            one_time_keyboard=True,
+        )
+
+    @property
+    def cancel_btn(self):
+        return ReplyKeyboardMarkup(
+            keyboard=[KeyboardButton(text=button.value) for button in CommonButtons],
+            resize_keyboard=True,
+            one_time_keyboard=True,
+        )
+
+    @property
+    def settings_menu_btn(self):
+        buttons = [KeyboardButton(text=button.value) for button in SettingsButtons]
         return ReplyKeyboardMarkup(
             keyboard=[
-                [KeyboardButton(text=value) for key, value in INPUT_METHODS.items()]
+                buttons[:2],
+                buttons[2:],
             ],
-            resize_keyboard=True,
-            one_time_keyboard=True,
-        )
-
-    @property
-    def confirm_input(self):
-        return ReplyKeyboardMarkup(
-            keyboard=[[KeyboardButton(text=value) for key, value in ANSWERS.items()]],
-            resize_keyboard=True,
-            one_time_keyboard=True,
-        )
-
-    @property
-    def cancel(self):
-        return ReplyKeyboardMarkup(
-            keyboard=[[KeyboardButton(text="❌ Отмена")]],
             resize_keyboard=True,
             one_time_keyboard=True,
         )
